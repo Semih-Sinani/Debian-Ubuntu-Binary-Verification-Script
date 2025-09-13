aca273859bf7a92bcbaf7097e5075927c45178c9
Semih Sinani <semche@ws-core.semche>
2025-09-13 13:14:28 +0200

    Add file list and package mapping for verification

---
+++ check_hash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+echo "== Start: Package/binary verification =="
+
+
+FILES=(
+    #example 
+
+#   "/usr/bin/passwd"
+#   "/usr/bin/chfn"
+#   "/opt/google/chrome/chrome-sandbox"
+#   "/usr/share/code/chrome-sandbox"
+#   "/usr/lib/virtualbox/VBoxHeadless"
+
+)
+
+declare -A PKG
+PKG["/usr/bin/passwd"]="passwd"
+PKG["/usr/bin/chfn"]="passwd"
+PKG["/opt/google/chrome/chrome-sandbox"]="google-chrome-stable"
+PKG["/usr/share/code/chrome-sandbox"]="code"
+PKG["/usr/lib/virtualbox/VBoxHeadless"]="virtualbox-7.2"
